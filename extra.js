2f53bab115a7c1671a33c1391a533c0a45ea2a18
Luke GB <git@lukegb.com>
2012-10-25 22:10:20 +0200

    Update extra.js

--- extra.js
+++ extra.js
@@ -7,8 +7,10 @@ window.onload = function() {
     el.innerHTML = '<h1>Hey there!</h1><p>You seem to have reached our Jenkins instance, whether on purpose or by accident.</p><p>Jenkins is no longer publically accessible, and you will be automatically redirected to our downloads page in <span id="overlord_counter">15</span> second<span id="overlord_counter_plural">s</span>.</p>';
     document.body.appendChild(el);
     el.id = 'overlord';
+    el.className = 'overlord';
     var counter = 15;
     var counterEl = document.getElementById('overlord_counter');
+    counterEl.className = 'overlord_counter';
     goAwayCounter = setInterval(function() {
       counter -= 1;
       counterEl.innerHTML = counter;
