9a90bdd0d741c7d45a6d46123a11936054cd5f72
Luke GB <git@lukegb.com>
2012-10-25 21:49:18 +0200

    Update extra.js

---
+++ extra.js
@@ -0,0 +1,4 @@
+if (document.getElementById('j_username') !== null && document.getElementById('j_password') !== null) {
+  // we have a username field. this is probably the login page.
+  console.log('login page');
+}
