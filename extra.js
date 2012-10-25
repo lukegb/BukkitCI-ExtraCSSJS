7022dd53d90f44c4d64deef84c9b0e9af9e6800b
Luke GB <git@lukegb.com>
2012-10-25 22:08:06 +0200

    Update extra.js

--- extra.js
+++ extra.js
@@ -4,9 +4,9 @@ window.onload = function() {
   if (document.getElementById('j_username') !== null) {
     // we have a username field. this is probably the login page.
     var el = document.createElement('div');
-    el.id = 'overlord';
     el.innerHTML = '<h1>Hey there!</h1><p>You seem to have reached our Jenkins instance, whether on purpose or by accident.</p><p>Jenkins is no longer publically accessible, and you will be automatically redirected to our downloads page in <span id="overlord_counter">15</span> seconds.</p>';
     document.body.appendChild(el);
+    el.id = 'overlord';
     var counter = 15;
     var counterEl = document.getElementById('overlord_counter');
     goAwayCounter = setInterval(function() {
